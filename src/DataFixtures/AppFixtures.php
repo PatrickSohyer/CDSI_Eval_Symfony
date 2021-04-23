167557538beaed71c190c3f5a8d125145a4c0925
Patrick Sohyer <p.sohyer@gmail.com>
2021-04-23 11:32:03 +0200

    ajout fixture seance

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,9 +2,10 @@
 
 namespace App\DataFixtures;
 
-use App\Entity\Formation;
-use App\Entity\Module;
 use Faker\Factory;
+use App\Entity\Module;
+use App\Entity\Seance;
+use App\Entity\Formation;
 use App\Entity\Utilisateur;
 use Doctrine\Persistence\ObjectManager;
 use Doctrine\Bundle\FixturesBundle\Fixture;
@@ -21,7 +22,9 @@ class AppFixtures extends Fixture
     public function load(ObjectManager $manager)
     {
         $faker = Factory::create('FR-fr');
-        for ($a = 1; $a < 5; $a++) {
+
+        // Fixtures pour créer des formateurs
+        for ($i = 1; $i < 5; $i++) {
             $userForma = new Utilisateur;
             $userForma->setEmail($faker->email);
             $userForma->setRoles(['ROLE_FORMATEUR']);
@@ -30,30 +33,42 @@ class AppFixtures extends Fixture
             $password = $this->encoder->encodePassword($userForma, 'aden');
             $userForma->setPassword($password);
             $manager->persist($userForma);
-            for ($b = 1; $b < 5; $b++) {
-                $userEleve = new Utilisateur;
-                $userEleve->setEmail($faker->email);
-                $userEleve->setRoles(['ROLE_ETUDIANT']);
-                $userEleve->setNom($faker->firstName);
-                $userEleve->setPrenom($faker->lastName);
-                $password = $this->encoder->encodePassword($userEleve, 'aden');
-                $userEleve->setPassword($password);
-                $manager->persist($userEleve);
-                for ($c = 1; $c < 5; $c++) {
-                    $forma = new Formation;
-                    $forma->setNom($faker->words(5, true));
-                    $forma->setDateDebut($faker->dateTimeBetween('+0 days', '+1 years'));
-                    $forma->setDateFin($faker->dateTimeBetween('+2 years', '+5 years'));
-                    $forma->getUtilisateurs([$userEleve]);
-                    $manager->persist($forma);
+        }
 
-                    for ($d = 1; $d <= mt_rand(6, 10); $d++) {
-                        $module = new Module;
-                        $module->setNom($faker->words(5, true));
-                        $module->setNbHeures($faker->numberBetween(10, 50));
-                        $module->setFormation($forma);
-                        $manager->persist($module);
-                    }
+        // Fixtures pour créer des étudiants
+        for ($i = 1; $i < 5; $i++) {
+            $userEleve = new Utilisateur;
+            $userEleve->setEmail($faker->email);
+            $userEleve->setRoles(['ROLE_ETUDIANT']);
+            $userEleve->setNom($faker->firstName);
+            $userEleve->setPrenom($faker->lastName);
+            $password = $this->encoder->encodePassword($userEleve, 'aden');
+            $userEleve->setPassword($password);
+            $manager->persist($userEleve);
+        }
+
+        // Fixtures pour créer des formations
+        for ($i = 1; $i < 5; $i++) {
+            $forma = new Formation;
+            $forma->setNom($faker->words(5, true));
+            $forma->setDateDebut($faker->dateTimeBetween('+0 days', '+1 years'));
+            $forma->setDateFin($faker->dateTimeBetween('+2 years', '+5 years'));
+            $manager->persist($forma);
+            // Fixtures pour créer des modules
+            for ($j = 1; $j <= mt_rand(6, 10); $j++) {
+                $module = new Module;
+                $module->setNom($faker->words(5, true));
+                $module->setNbHeures($faker->numberBetween(10, 50));
+                $module->setFormation($forma);
+                $manager->persist($module);
+                for ($k = 1; $k <= mt_rand(1, 10); $k++) {
+                    $seance = new Seance;
+                    $seance->setDateSeance($faker->dateTimeBetween('+100 days', '+3 years'));
+                    $seance->setDuree($faker->randomFloat(2, 1, 4));
+                    $seance->setTitre($faker->words(3, true));
+                    $seance->setContenu($faker->paragraphs(2, true));
+                    $seance->setModule($module);
+                    $manager->persist($seance);
                 }
             }
         }
